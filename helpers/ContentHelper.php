5c8ecdf463a5ff4833ae5b776a5600343f833045
root-- <eugene.anufriev@gmail.com>
2016-08-15 16:50:07 +0300

    rebuild helper

--- helpers/ContentHelper.php
+++ helpers/ContentHelper.php
@@ -6,24 +6,51 @@ use Yii;
 
 class ContentHelper
 {
-    public static function fetchImages($content)
+    /**
+     * @param string $content
+     * @param string $mainId
+     * @param string $defaultSrc
+     * @return array
+     */
+    public static function fetchImages($content, $mainId = null ,$defaultSrc = null)
     {
+        $mainId = ($mainId) ? $mainId : 'main';
+        $images = [];
+        $c = 0;
+
         preg_match_all("#<img(.*?)\/?>#", $content, $matches);
         // extract attributes from each image and place in $images array
-        $images = [];
         foreach ($matches[1] as $m) {
             preg_match_all("#(\w+)=['\"]{1}([^'\"]*)#", $m, $matches2);
-            // code below could be a lot neater using array_combine(), but it's php5 only
+
             $tempArray = [];
-            foreach($matches2[1] as $key => $val) {
+            foreach ($matches2[1] as $key => $val) {
                 $tempArray[$val] = $matches2[2][$key];
             }
-//            if ($tempArray['id']=='headImage'){ $headImage=$tempArray['src'];}
-//            if ($tempArray['id']=='newsImage'){ $newsImage=$tempArray['src'];}
-
-            return $tempArray;
+            // detect main image
+            if (isset($tempArray['id'])) {
+                if ($tempArray['id'] == $mainId) {
+                    $main = $tempArray;
+                }
+            }
+            $c++;
+            $images[$c] = $tempArray;
         }
 
+        // fill default main image
+        if (empty($main)) {
+            // by first
+            if (!empty($images[1])) {
+                $main = $images[1];
+            } else {
+                $main['src'] = ($defaultSrc)?$defaultSrc: 'http://placehold.it/400x250';
+                $main['alt'] = 'no image here';
+            }
+        }
+        $out['images'] = $images;
+        $out['count'] = $c;
+        $out['main'] = $main;
+        return $out;
     }
 
 }
